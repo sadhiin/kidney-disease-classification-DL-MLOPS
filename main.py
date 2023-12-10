06be7903d1fe8a6cc055fe75afdf9cbdebd3ce15
sadhiin <sanjidulislamsadhin1994@gmail.com>
2023-12-11 01:45:48 +0600

    Bug Fix

--- main.py
+++ main.py
@@ -3,22 +3,22 @@ from kidneyDiseaseClassifier.pipeline.stage_01_data_ingestion import DataIngesti
 from kidneyDiseaseClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from kidneyDiseaseClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
 
-STAGE_NAME = "Data Ingestion Stage"
-
-try:
-    logger.info(f">>>>>>>>> At stage {STAGE_NAME} started <<<<<<<<<")
-    obj = DataIngestionTrainingPipeline()
-    which_one = input("Inter your choice. Press 'g' or 'G' for Google, 'k' or 'K' for kaggle: ")
-    if which_one.lower() == 'k':
-        logger.info(f"Downloading data from the kaggle")
-        obj.main(use_kaggle=True)
-    elif which_one.lower() == 'g':
-        logger.info(f"Downloading data from the kaggle")
-        obj.main(use_gdrive=True)
-except Exception as e:
-    logger.exception(e)
-    raise e
-
+# STAGE_NAME = "Data Ingestion Stage"
+#
+# try:
+#     logger.info(f">>>>>>>>> At stage {STAGE_NAME} started <<<<<<<<<")
+#     obj = DataIngestionTrainingPipeline()
+#     which_one = input("Inter your choice. Press 'g' or 'G' for Google, 'k' or 'K' for kaggle: ")
+#     if which_one.lower() == 'k':
+#         logger.info(f"Downloading data from the kaggle")
+#         obj.main(use_kaggle=True)
+#     elif which_one.lower() == 'g':
+#         logger.info(f"Downloading data from the kaggle")
+#         obj.main(use_gdrive=True)
+# except Exception as e:
+#     logger.exception(e)
+#     raise e
+#
 
 STAGE_NAME = "Prepare base model"
 
