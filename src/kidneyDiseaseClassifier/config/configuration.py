4b45743936d7783e9d1206b94e094bb9171a192e
sadhiin <sanjidulislamsadhin1994@gmail.com>
2023-12-11 01:35:49 +0600

    Model training configuration manager added

--- src/kidneyDiseaseClassifier/config/configuration.py
+++ src/kidneyDiseaseClassifier/config/configuration.py
@@ -1,6 +1,7 @@
+import os
 from kidneyDiseaseClassifier.constants import *
 from kidneyDiseaseClassifier.utils.common import read_yaml, create_directories
-from kidneyDiseaseClassifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig
+from kidneyDiseaseClassifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig, TrainingConfig
 
 
 class ConfigurationManager:
@@ -66,3 +67,28 @@ class PretrainModelConfigurationManager:
         )
 
         return prepared_base_model_config
+
+    def get_training_config(self) -> TrainingConfig:
+        training = self.config.model_training
+        prepare_base_model = self.config.prepare_base_model
+        params = self.params
+
+        training_data = os.path.join(self.config.data_ingestion_kaggle.local_data_file,
+                                     "CT-KIDNEY-DATASET-Normal-Cyst-Tumor-Stone")
+        create_directories([Path(training.root_dir)])
+
+        # logger.info(f"training data path: {training_data}")
+        # logger.info(f"{os.listdir(training_data)}")
+
+        training_config = TrainingConfig(
+            root_dir=Path(training.root_dir),
+            trained_model_path=Path(training.trained_model_path),
+            updated_base_model_path=Path(prepare_base_model.updated_base_model_path),
+            training_data=Path(training_data),
+            params_epochs=params.EPOCHS,
+            params_batch_size=params.BATCH_SIZE,
+            params_is_augmentation=params.AUGMENTATION,
+            params_image_size=params.IMAGE_SIZE
+        )
+
+        return training_config
